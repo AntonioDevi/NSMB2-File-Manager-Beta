8e8f1d59fd6947705078d2671d50c1271a5a22cb
ant888 <anewhope8@outlook.com>
2015-08-24 13:05:45 -0400

    Created Win32 file
    And other stuff
    Need to edit it

--- Win32.cpp
+++ Win32.cpp
@@ -41,7 +41,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","New Super Mario Bros. 2 File Manager",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
 		640, /* width */
